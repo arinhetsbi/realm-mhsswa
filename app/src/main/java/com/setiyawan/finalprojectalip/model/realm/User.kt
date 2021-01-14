624101fd05cf7345707d7faf9a2ed5dac0d1342e
arinhetsbi <you@example.com>
2021-01-14 13:08:40 +0700

    arn android

---
+++ app/src/main/java/com/setiyawan/finalprojectalip/model/realm/User.kt
@@ -0,0 +1,36 @@
+package com.setiyawan.finalprojectalip.model.realm
+
+import io.realm.RealmObject
+import io.realm.annotations.RealmClass
+ @RealmClass
+    open class User: RealmObject() {
+        private var id: Int=0
+        private var nama:String=""
+        private var email:String=""
+        private var alamat:String=""
+
+        fun setId(id:Int){
+            this.id =id
+        }
+        fun getId():Int{
+            return id
+        }
+        fun setNama(nama:String){
+            this.nama=nama
+        }
+        fun getNama():String{
+            return nama
+        }
+        fun setEmail(email:String){
+            this.email=email
+        }
+        fun getEmail():String{
+            return email
+        }
+        fun setAlamat(alamat : String){
+            this.alamat = alamat
+        }
+        fun  getAlamat ():String{
+            return alamat
+        }
+    }
